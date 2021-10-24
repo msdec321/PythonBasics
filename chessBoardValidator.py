81f4c76e75312efed413d007013ce7d8b08f8ab2
msdec321 <msdec321@gmail.com>
2021-10-24 13:03:12 -0400

    Cleaned typos.

--- chessBoardValidator.py
+++ chessBoardValidator.py
@@ -1,10 +1,10 @@
 #This is the chess board validator script.
 #This checks if the state of the chess board is valid or not.
-#A black king (bking) and white king (wking) should be present, and
-# opposing pieces should not overlap on the same tile.
+#(Exactly 1 black and white king, no more than 8 pawns per player,
+#  no more than 16 pieces per player.)
 
 tiles = {'1a': '', '1b': '', '1c': '', '1d': '', '1e': '', '1f': '', '1g': '', '1h': '',
-	     '2a': '', '2b': '', '2c': '', '2d': '', '2e': '', '2f': '', '2g': '', '2h': '',
+	 '2a': '', '2b': '', '2c': '', '2d': '', '2e': '', '2f': '', '2g': '', '2h': '',
          '3a': '', '3b': '', '3c': '', '3d': '', '3e': '', '3f': '', '3g': '', '3h': '',
          '4a': '', '4b': '', '4c': '', '4d': '', '4e': '', '4f': '', '4g': '', '4h': '',
          '5a': '', '5b': '', '5c': '', '5d': '', '5e': '', '5f': '', '5g': '', '5h': '',
